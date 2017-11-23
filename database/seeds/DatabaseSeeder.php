4d2584cdb7951b14ff377055163cccc3856d156f
Harrison Favour <fayvorharrison101@gmail.com>
2017-11-23 13:28:30 +0100

    added some seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,5 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([[
+            'id' => 1,
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'username' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => '$2y$10$1uaX5Se8JhxV8R1GZhp.AOa.rIaDzdvLQ4At4rOLGk9yPeS7pE3FK',
+            'account_type' => 'Admin',
+            'image_location' => '',
+            'identification_number' => '2343435DFDD'
+        ]]);
+        DB::table('users')->insert([[
+            'id' => 2,
+            'first_name' => 'mechanic',
+            'last_name' => 'mechanic',
+            'username' => 'mechanic',
+            'email' => 'fayvorharrison101@gmail.com',
+            'password' => '$2y$10$1uaX5Se8JhxV8R1GZhp.AOa.rIaDzdvLQ4At4rOLGk9yPeS7pE3FK',
+            'account_type' => 'Mechanic',
+            'image_location' => '',
+            'identification_number' => '23234DFDG34'
+        ]]);
+        DB::table('mechanics')->insert([[
+            'id' => 1,
+            'user_id' => 2,
+            'status' => 'Ready'
+        ]]);
     }
 }
