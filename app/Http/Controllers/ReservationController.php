c7241dfc8ee355cf47ed0340a6f0c820649e63a7
Harrison Favour <fayvorharrison101@gmail.com>
2017-12-13 09:20:32 +0100

    logged found mechanic data

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -96,7 +96,7 @@ class ReservationController extends Controller
         $user = $request->user();
         return "Mechanic" == $user->account_type
             ? $this->service->removeReservationMechanic($request)
-            : response()->json(['message' => 'not allowed to perform this operation'], 403);
+            : response()->json(['message' => 'not allowed to perform this operation', 'data' => $user], 403);
     }
 
 }
